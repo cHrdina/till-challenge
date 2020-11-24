5e925a8ca1f4ca51b7a306a40f72bc243117f6c0
chrdina <christian_hrdina@hotmail.com>
2020-11-24 14:06:10 +1100

    add working functionality to merchant picker

--- src/components/MerchantPicker.tsx
+++ src/components/MerchantPicker.tsx
@@ -7,17 +7,36 @@ type Props = {
 };
 
 const MerchantPicker: React.FC<Props> = ({ merchants }) => {
-  const [activeItem, setActiveItem] = useState('');
+  const [activeItem, setActiveItem] = useState<Merchant | undefined>(
+    merchants[0]
+  );
+
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = e.target.value;
+    const selectedItem = merchants.find(
+      (merchant) => merchant.id === selectedId
+    );
+
+    setActiveItem(selectedItem);
+  };
 
   return (
     <div className="picker-container">
       <div>
         <label htmlFor="merchants">Select a Merchant: </label>
-        <select name="merchants" id="merchants" className="dropdown">
+        <select
+          name="merchants"
+          id="merchants"
+          className="dropdown"
+          onChange={(e) => handleSelect(e)}
+        >
           {merchants.map((merchant) => (
             <option value={merchant.id}>{merchant.name}</option>
           ))}
         </select>
+      </div>
+      <div className="merchant-picker-results">
+        {activeItem && <MerchantComponent {...activeItem} />}
       </div>
     </div>
   );
---
+++ src/components/StatusTag.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const StatusTag: React.FC<Props> = ({}) => {
+  return <div className="status-tag-container"></div>;
+};
+
+export default StatusTag;
